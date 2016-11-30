a41f4301af3f89dce39e3ae959f7c6cfc0514a14
yurchik <15_yurchik@ukr.net>
2016-11-30 22:08:01 +0200

    Add some code fore exeutable create and autoran.inf file

---
+++ setup.py
@@ -0,0 +1,15 @@
+import sys
+from cx_Freeze import setup, executable
+
+
+include_files = ['autorun.inf']
+base = None
+
+if sys.platform == "win32":
+	base = "Win32GUI"
+
+setup(name="puzzel",
+	  version="0.1",
+	  description="Fun computer game",
+	  options={'buil_exe': {'include_files': include_files}},
+	  executables=[Executable("client.by", base=base)])
